6fd9f54402e3561020c2ec18b222dbcf9a4d4e28
pad19 <pad19051999@gmail.com>
2020-08-10 16:38:15 +0300

    Add first version os slider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Carousel from './Carousel';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <Carousel slides={[
+        {
+            title: 'title',
+            description: 'description',
+            src: 'https://specials-images.forbesimg.com/imageserve/5d35eacaf1176b0008974b54/960x0.jpg?cropX1=790&cropX2=5350&cropY1=784&cropY2=3349',
+        },
+        {
+            title: 'title',
+            description: 'description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description ',
+            src: 'https://www.motoringresearch.com/wp-content/uploads/2019/09/01-fastest-cars.jpg',
+        },
+        {
+            title: 'title',
+            description: 'description',
+            src: 'https://i1.rozetka.ua/goods/3188797/disney_cars_fgn48_images_3188797151.jpg',
+        },
+    ]}/>
 }
 
 export default App;
---
+++ src/Carousel/index.jsx
@@ -0,0 +1,113 @@
+import React, {Component} from 'react';
+import Slide from './Slide';
+import styles from './Carousel.module.scss';
+import classNames from 'classnames';
+import Icon from '@mdi/react'
+import {mdiSkipPrevious, mdiSkipNext, mdiFullscreen, mdiFullscreenExit, mdiPlay, mdiPause} from '@mdi/js';
+
+class Carousel extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            delay: 1000,
+            isPlaying: false,
+            isFullscreen: false,
+            currentIndex: 0,
+        };
+        this.timeoutId = null;
+    }
+
+    nextIndex = () => {
+        const {currentIndex} = this.state;
+        const {slides} = this.props;
+        this.setState({
+            currentIndex: (currentIndex + 1) % slides.length,
+        });
+    }
+
+    prevIndex = () => {
+        const {currentIndex} = this.state;
+        const {slides} = this.props;
+        this.setState({
+            currentIndex: (currentIndex - 1 + slides.length) % slides.length,
+        });
+    }
+
+    slideshowHandler = () => {
+        const {isPlaying} = this.state;
+        this.setState({
+            isPlaying: !isPlaying,
+        })
+    }
+
+    delayHandler = (event) => {
+        this.setState({
+            delay: event.target.value,
+        })
+    }
+
+    fullscreenMode = () => {
+        if (document.fullscreenElement) {
+            document.exitFullscreen();
+            this.setState({
+                isFullscreen: false,
+            });
+        } else {
+            document.documentElement.requestFullscreen();
+            this.setState({
+                isFullscreen: true,
+            });
+        }
+    }
+
+    static getNextIndex(index, slides) {
+        return (index + 1) % slides.length;
+    }
+
+    static getPrevIndex(index, slides) {
+        return (index - 1 + slides.length) % slides.length;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        const {isPlaying, delay} = this.state;
+        clearTimeout(this.timeoutId);
+        this.timeoutId = null;
+        if (isPlaying) {
+            this.timeoutId = setTimeout(this.nextIndex, delay);
+        }
+    }
+
+    render() {
+        const {slides, download = true} = this.props;
+        const {currentIndex, isPlaying, delay, isFullscreen} = this.state;
+        const containerClass = classNames(styles.container, {
+            [styles.fullscreen]: isFullscreen,
+        });
+        return (
+            <article className={containerClass}>
+                {download && (
+                    <>
+                        <Slide download {...slides[currentIndex]} isCurrent isFullscreen={isFullscreen}/>
+                    </>
+                )}
+                <div className={styles.slideControl}>
+                    <div className={styles.delay}>
+                        <input type='range' value={delay} min={1} max={10000} onChange={this.delayHandler}/>
+                        <span>{delay}</span>
+                    </div>
+                    <div className={styles.nextPrev}>
+                        <Icon onClick={this.prevIndex} path={mdiSkipPrevious}/>
+                        <Icon onClick={this.nextIndex} path={mdiSkipNext}/>
+                    </div>
+                    <div className={styles.slideshow}>
+                        <Icon onClick={this.slideshowHandler} path={isPlaying ? mdiPause : mdiPlay}/>
+                        <Icon onClick={this.fullscreenMode} path={isFullscreen ? mdiFullscreenExit : mdiFullscreen}/>
+                    </div>
+                </div>
+            </article>
+        );
+    }
+}
+
+export default Carousel;
